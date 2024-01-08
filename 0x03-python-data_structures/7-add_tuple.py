471bb6cec3b8ad26d2ae1188d1617cc6e0cef7a6
sam <samnyoru4@gmail.com>
2024-01-08 15:08:17 -0800

    Tuples addition rectified

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a, tuple_b):
-    result = tuple(x + y for x, y in zip(tuple_a, tuple_b)) + tuple(tuple_a[len(tuple_b):] + tuple_b[len(tuple_a):])
-    return result
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1,)))
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a, len_b = len(tuple_a), len(tuple_b)
+    new_tuple = ((tuple_a[0] if len_a >= 1 else 0) +
+                 (tuple_b[0] if len_b >= 1 else 0),
+                 (tuple_a[1] if len_a >= 2 else 0) +
+                 (tuple_b[1] if len_b >= 2 else 0))
+    return new_tuple
 
