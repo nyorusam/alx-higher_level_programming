f882483f7a740c9ed5cac9bf3304d7f875944eff
sam <samnyoru4@gmail.com>
2024-01-12 12:07:21 -0800

    what am i getting wrong

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=None):
     if matrix is None:
-        matrix = [] 
+        matrix = []  
     squared = []
     for line in matrix:
         squared.append([c**2 for c in line])
